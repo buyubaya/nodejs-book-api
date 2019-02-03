99138eb8fab44bc624fc00c6b1759ab6aaf8a9f6
buyubaya <hihihi_caigikia@yahoo.com>
2019-02-03 19:36:10 +0700

    'HELLO'

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use('/api/product', bookRoute);
 app.use('/api/category', categoryRoute);
 app.use('/api/author', authorRoute);
 app.use('/api/brand', brandRoute);
-app.use('/user', userRoute);
+app.use('/api/user', userRoute);
 app.use('/', (req, res, next) => {
     res.send('HELLO BOOKS');
 });
